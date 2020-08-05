8b442517102aaa4bd2cc98000967c71a6cf3b3e3
Tamara Luque <tam.luque@gmail.com>
2020-08-04 22:08:17 -0400

    events

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const Product = ({product}) => {
-  const { name, price, id } = product
+  const { name, price, id } = product;
+
+  // Agregar producto al carrito
+  const selectProduct = id => {
+    console.log('comprando... ', id)
+  }
 
   return ( 
     <div>
       <h2>{name}</h2>
       <p>{price}</p>
+      <button
+        type="button"
+        onClick={ () => selectProduct(id) }
+      >
+        Comprar
+      </button>
     </div>
   );
 }

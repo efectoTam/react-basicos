a87d1bd089afbf0b2a4ed0328fbe315a8981f588
Tamara Luque <tam.luque@gmail.com>
2020-08-04 21:27:57 -0400

    other component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
+    <h1 className="encabezado">Tienda Virtual</h1>
   )
 }
 
